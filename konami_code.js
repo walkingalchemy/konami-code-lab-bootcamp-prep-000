07e94aa598978bf516896a2743de44eda5de221d
James Royer <walkingalchemy@gmail.com>
2017-09-30 19:27:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const input = document.querySelector('input')
+  let index = 0
+  //var entered = []
+  
+  input.addEventListner('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      i++
+      if (index === code.length) {
+        alert("+30 Lives!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 
 }
